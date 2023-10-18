f793a37599b5b052ab6fbd96ae0f5fa9db1c732b
Michael <30883926+mgalde@users.noreply.github.com>
2023-10-18 14:36:31 -0700

    Create hexcheck.js

---
+++ hexcheck.js
@@ -0,0 +1,42 @@
+// Note: This is a very basic check, more robust methods would involve AJAX, Fetch API, etc.
+
+function checkServerStatus(serverIp, callback) {
+    var img = new Image();
+    img.onload = function() {
+        callback(true);
+    };
+    img.onerror = function() {
+        callback(false);
+    };
+
+    // This assumes servers will respond to a ping or request.
+    img.src = "http://" + serverIp;
+}
+
+function checkServiceStatus(serverIp, servicePort, callback) {
+    // Using fetch or AJAX to check if a service is running can be a bit more involved 
+    // due to potential CORS issues. Here's a basic method:
+    fetch("http://" + serverIp + ":" + servicePort)
+    .then(response => {
+        if (response.ok) callback(true);
+        else callback(false);
+    })
+    .catch(error => {
+        callback(false);
+    });
+}
+
+// Example usage:
+checkServerStatus("192.168.1.1", function(isOnline) {
+    document.getElementById("server1-status").textContent = isOnline ? "Online" : "Offline";
+    document.getElementById("server1-status").className = isOnline ? "online" : "offline";
+
+    if (isOnline) {
+        checkServiceStatus("192.168.1.1", "8080", function(isServiceRunning) {
+            document.getElementById("server1-service").textContent = isServiceRunning ? "Running" : "Not Running";
+            document.getElementById("server1-service").className = isServiceRunning ? "online" : "offline";
+        });
+    }
+});
+
+// Repeat for other servers...
